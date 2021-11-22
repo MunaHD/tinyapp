15dfaf380e29bcfa4edb3134818a8cc8900f9197
Muna Dahir <muna.dahir@hotmail.com>
2021-11-21 20:03:49 -0500

    fixed the post/urls and post/urls/:id permission issues

--- express_server.js
+++ express_server.js
@@ -37,15 +37,7 @@ app.get("/", (req, res) => {
 });
 
 
-
-app.get("/home", (req, res) => { 
-  const userId = req.session.user_id 
-
-  const templateVariables = {user: users[userId]}
-  res.render("home", templateVariables);
-})
-
-
+//register page
 app.get("/register", (req,res) => {
   const userId = req.session.user_id 
   //if user is logged in redirect to urls
@@ -57,6 +49,75 @@ app.get("/register", (req,res) => {
   res.render("register", templateVariables);
 });
 
+
+//login page
+app.get("/login", (req, res) => { 
+  const userId = req.session.user_id 
+   //if user is logged in redirect to urls
+   if (userId) {
+    return res.redirect("/urls")
+  }
+  const templateVariables = {user: users[userId]}
+  res.render("login", templateVariables);
+});
+
+
+//shows the page to create new short url
+app.get("/urls/new", (req, res) => {
+   //if user is looged in redirect to urls
+   const userId = req.session.user_id 
+   if (!userId) {
+    return res.redirect("/login")
+  }
+  
+  const templateVariables = {user: users[userId]}
+  res.render("urls_new", templateVariables);
+});
+
+
+//create link that shortURL leads to
+app.get("/u/:shortURL", (req, res) => {
+  const shortUrl = req.params.shortURL
+  
+  if (!urlDatabase[shortUrl]) {
+    return res.status(404).send("The shortURL you are trying to reach does not exist")
+  }
+
+  const longURL = urlDatabase[shortUrl].longURL
+  res.redirect(longURL);
+});
+
+
+//shows the page that lists urls
+app.get("/urls", (req, res) => {
+   const userId = req.session.user_id 
+  
+
+   //if user is looged in redirect to urls
+   if (!userId) {
+    return res.status(401).send("Please <a href='/login'>login</a> before accessing this page!")
+  }
+  
+
+  const templateVariables = {user: users[userId], urls: urlsForUser(userId) };
+  res.render("urls_index", templateVariables);
+});
+
+
+//shows short url page
+app.get("/urls/:shortURL", (req, res) => {
+  const userId = req.session.user_id 
+  const shortURL = req.params.shortURL; 
+
+
+  const templateVariables = {
+    user: users[userId],
+    shortURL: shortURL , 
+    longURL: urlDatabase[shortURL].longURL 
+  };
+  
+  res.render("urls_show", templateVariables);
+});
 
 
 app.post("/register", (req, res) => {
@@ -70,7 +131,7 @@ app.post("/register", (req, res) => {
     return res.status(400).send('Email/Password is empty. Please try again!');
   }
   
-  //console.log("user email", user.email)
+  // if the email
   if(findUserbyEmail(email, users)) {
     return res.status(400).send(' A user already exists with this email!');
   }
@@ -91,171 +152,6 @@ app.post("/register", (req, res) => {
   
   res.redirect("/register")
 
-});
-
-
-
-
-
-
-//shows the page to create new short url
-app.get("/urls/new", (req, res) => {
-   //if user is looged in redirect to urls
-   const userId = req.session.user_id 
-   if (!userId) {
-    return res.redirect("/login")
-  }
-  
-  const templateVariables = {user: users[userId]}
-  res.render("urls_new", templateVariables);
-});
-
-
-
-
-
-//create link that shortURL leads to
-app.get("/u/:shortURL", (req, res) => {
-  const shortUrl = req.params.shortURL
-  
-  if (!urlDatabase[shortUrl]) {
-    return res.status(404).send("The shortURL you are trying to reach does not exist")
-  }
-
-  const longURL = urlDatabase[shortUrl].longURL
-  res.redirect(longURL);
-});
-
-
-
-
-
-
-//shows the page that lists urls
-app.get("/urls", (req, res) => {
-   const userId = req.session.user_id 
-   //if user is looged in redirect to urls
-   if (!userId) {
-    return res.status(401).send("Please <a href='/login'>login</a> before accessing this page!")
-  }
-
-  const templateVariables = {user: users[userId], urls: urlsForUser(userId) };
-  res.render("urls_index", templateVariables);
-});
-//create shortURL
-
-    
-
-
-app.post("/urls", (req, res) => {
-  const userId = req.session.user_id 
-  let shortURL = generateRandomString()
-  //generates a shortURL associated with longURL and user
-  urlDatabase[shortURL]= {longURL: req.body.longURL, userID: userId }
-  res.redirect(`/urls/${shortURL}`) 
-  
-});
-
-
-
-
-
-
-
-//shows short url page
-app.get("/urls/:shortURL", (req, res) => {
-  const userId = req.session.user_id 
-  const shortURL = req.params.shortURL; 
-
-   //if user is not logged in
-   if (!userId ) {
-    
-    return res.status(401).send("Please <a href='/login'>login</a> before accessing this page")
-  }
-  
-  //if the shortURL does not exist
-   if (!urlDatabase[shortURL]) {
-     
-    return res.status(404).send("ShortURL does not exist")
-   }
-
-  //if user is logged in doesnt own url 
-  if(userId !== urlDatabase[shortURL].userID) {
-      return res.status(403).send("This shortURL does not belong to you!")
-  
-  }
-
-  const templateVariables = {
-    user: users[userId],
-    shortURL: shortURL , 
-    longURL: urlDatabase[shortURL].longURL 
-  };
-  
-  res.render("urls_show", templateVariables);
-});
-
-
-
-
-
-//updates the long url and saves it 
-app.post("/urls/:shortURL", (req, res) => {
-  const userId = req.session.user_id 
-  const shortURL = req.params.shortURL;
-
-  //if user isn't logged in return Error
-  if (!userId ) {
-    return res.status(401).send("Please <a href='/login'>login</a> before accessing this page")
-  }
-   //if user is looged in  but doesn't own URL return error message
-   if (userId && userId !== urlDatabase[shortURL].userID) {
-    res.status(401).send('Can\'t edit URL because you do not own it')
-  }
-
-  //update the longURL
-  urlDatabase[shortURL] = {longURL: req.body.longURL, userID: userId }
-  res.redirect("/urls")
-});
-
-
-
-
-
-
-
-//delete urls
-app.post("/urls/:shortURL/delete", (req, res)=> {
-  const userId = req.session.user_id 
-  const shortURL = req.params.shortURL;
-  
-  //if user isn't loged in 
-  if (!userId) {
-    return res.status(401).send('Can\'t delete URL because you are not logged in!')
-  }
-  
-  //if user is logged in but soen't own URL
-  if (userId && userId !== urlDatabase[shortURL].userID) {
-    return res.status(401).send('Can\'t delete URL because you do not own it')
-  }
-
-  delete urlDatabase[shortURL];
-
-   res.redirect("/urls")
-});
-
-
-
-
-
-
-app.get("/login", (req, res) => { 
-  const userId = req.session.user_id 
-   //if user is logged in redirect to urls
-   if (userId) {
-    return res.redirect("/urls")
-  }
-  const templateVariables = {user: users[userId]}
-  res.render("login", templateVariables);
 });
 
 
@@ -289,7 +185,79 @@ app.post("/login", (req, res)=> {
 });
 
 
+//create shortURL
+app.post("/urls", (req, res) => {
+  const userId = req.session.user_id 
+  let shortURL = generateRandomString()
+  
+    //if user is not logged in
+    if (!userId ) {
+      return res.status(401).send("Please <a href='/login'>login</a> before accessing this page")
+    } else {
+      urlDatabase[shortURL]= {longURL: req.body.longURL, userID: userId }
+      res.redirect(`/urls/${shortURL}`)
+    }
+    
+    //if the shortURL does not exist
+     if (!urlDatabase[shortURL]) {
 
+       
+      return res.status(404).send("ShortURL does not exist")
+     }
+  
+    //if user is logged in doesnt own url 
+    if(userId !== urlDatabase[shortURL].userID) {
+
+        return res.status(403).send("This shortURL does not belong to you!")
+    
+    }
+  
+   
+  
+});
+
+
+//updates the long url and saves it 
+app.post("/urls/:shortURL", (req, res) => {
+  const userId = req.session.user_id 
+  const shortURL = req.params.shortURL;
+
+  //if user isn't logged in return Error
+  if (!userId ) {
+    return res.status(401).send("Please <a href='/login'>login</a> before accessing this page")
+  }
+   //if user is logged in  but doesn't own URL return error message
+   if (userId && (userId === urlDatabase[shortURL].userID)) {
+     //updates the shortURL
+    urlDatabase[shortURL]= {longURL: req.body.longURL, userID: userId } 
+    return res.redirect("/urls")
+    
+    } else {
+      return res.status(401).send('Can\'t edit URL because you do not own it')
+    }
+  
+});
+
+
+//delete urls
+app.post("/urls/:shortURL/delete", (req, res)=> {
+  const userId = req.session.user_id 
+  const shortURL = req.params.shortURL;
+  
+  //if user isn't loged in 
+  if (!userId) {
+    return res.status(401).send('Can\'t delete URL because you are not logged in!')
+  }
+  
+  //if user is logged in but soen't own URL
+  if (userId && userId !== urlDatabase[shortURL].userID) {
+    return res.status(401).send('Can\'t delete URL because you do not own it')
+  }
+
+  delete urlDatabase[shortURL];
+
+   res.redirect("/urls")
+});
 
 
 //delete cookie (user
